63b9fbf0626be1a79164afde7c218f8ec23d5278
Aman78600 <105182914+Aman78600@users.noreply.github.com>
2024-07-11 13:16:50 +0530

    app.py

--- app.py
+++ app.py
@@ -28,6 +28,21 @@ def text_transform(text):
 tfidf=pickle.load(open('transformar.pkl','rb'))
 model=pickle.load(open('model.pkl','rb'))
 
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('SMS_img.jpeg')  
 
 st.header("SMS Spam Prediction.")
 input_data=st.text_area('Enter SMS.')
